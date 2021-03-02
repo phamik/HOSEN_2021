a9d9060af8bdab679c27e97244b75204f0238d3b
Tien Pham <tmpham95@gmail.com>
2021-03-02 16:50:23 +0000

    footer space between items

--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -7,22 +7,26 @@ export const FooterContainer = styled.div`
 	color: ${props => props.theme.color.gold};
 	bottom: 0;
 	align-items: center;
-	justify-content: center;
+	justify-content: space-between;
 	font-weight: ${props => props.theme.fontWeight.bold};
 	padding: 0 1.45rem;
 
 	@media (max-width: ${props => props.theme.media.small}) {
     flex-direction: column;
   }
+
+	& div ::before {
+  	content: "";
+	}
+
+	& div ::after {
+		content: "";
+	}
 `;
 
 export const OpeningTimesContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	margin-right: 5rem;
-	@media (max-width: ${props => props.theme.media.small}) {
-    margin-right: 0;
-  }
 `;
 
 export const LogoContainer = styled.div`
@@ -30,12 +34,7 @@ export const LogoContainer = styled.div`
   width: 20%;
   top: 0;
   left: 0;
-	margin-right: 5rem;
 	text-align: center;
-	
-	@media (max-width: ${props => props.theme.media.small}) {
-    margin-right: 0;
-  }
 `;
 
 export const AddressContainer = styled.div`
